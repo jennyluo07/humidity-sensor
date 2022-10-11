c4b3ff3e277786c3849d2b69942ccc369f24cd20
jennyluo07 <115515914+jennyluo07@users.noreply.github.com>
2022-10-10 22:04:48 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-basic.forever(function on_forever() {
-    dht11_dht22.queryData(DHTtype.DHT11, DigitalPin.P0, true, false, true)
-    if ((0 as any) == (0 as any)) {
+basic.forever(function () {
+    dht11_dht22.queryData(
+    DHTtype.DHT11,
+    DigitalPin.P0,
+    true,
+    false,
+    true
+    )
+    if (0 == 0) {
         basic.showNumber(dht11_dht22.readData(dataType.humidity))
     }
-    
 })
